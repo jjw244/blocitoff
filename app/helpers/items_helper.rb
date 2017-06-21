a29a8aa30c2194313c5ac1702a54fa962dd1f0cb
Jennifer washburn <jjw244@users.noreply.github.com>
2017-06-21 16:24:35 -0500

    User Story - 8 - Delete Expired ToDo Items - COMPLETE

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,6 +1,6 @@
 module ItemsHelper
-  
+
   def days_left(item)
-    distance_of_time_in_words(7.days.ago, item.created_at)
+    distance_of_time_in_words(Time.now, item.created_at + 7.days)
   end
 end

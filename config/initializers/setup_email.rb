32bda69c63071ca45f5b6f66ace4ab45fcf2eff3
Jennifer washburn <jjw244@users.noreply.github.com>
2017-06-23 12:00:13 -0500

    Removed dev environment from setup_email.rb and added application.yml to master

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -1,27 +1,10 @@
-if Rails.env.development?
-
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address:               "smtp.gmail.com",
-    port:                  587,
-    domain:                'gmail.com',
-    user_name:             Figaro.env.gmail_username_dev,
-    password:              Figaro.env.gmail_password_dev,
-    authentication:        'plain',
-    enable_starttls_auto:  true
-  }
-
-elsif Rails.env.production?
-
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address:                'smtp.sendgrid.net',
-    port:                   587,
-    domain:                 'heroku.com',
-    user_name:              ENV['SENDGRID_USERNAME'],
-    password:               ENV['SENDGRID_PASSWORD'],
-    authenitcation:         'plain',
-    enable_starttls_auto:   true
-  }
-
-end
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address:                'smtp.sendgrid.net',
+  port:                   587,
+  domain:                 'heroku.com',
+  user_name:              ENV['SENDGRID_USERNAME'],
+  password:               ENV['SENDGRID_PASSWORD'],
+  authenitcation:         'plain',
+  enable_starttls_auto:   true
+}
